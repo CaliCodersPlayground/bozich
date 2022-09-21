b8adfb09b060c6d29f260f99b1bdd195f5681afb
Kirk Clemons <kirk.clemons@gmail.com>
2022-09-21 12:25:16 -0700

    Updating background scaling.

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,7 @@ function backgroundSequence() {
     for (i = 0; i < bgImageArray.length; i++) {
         setTimeout(function(){
             document.getElementById("hero").style.background = "url('" + base + bgImageArray[k] + "') no-repeat center center";
-            document.getElementById("hero").style.backgroundSize = "cover";
+            document.getElementById("hero").style.backgroundSize = "1920px 1080px";
             if ((k + 1) === bgImageArray.length) {
                 setTimeout(function() {
                     backgroundSequence()
